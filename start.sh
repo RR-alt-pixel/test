f7deffae17b98d6be04d8c3b89aabe56c419a3a0
RR-alt-pixel <admiin2025@list.ru>
2025-10-17 16:10:58 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-echo "🚀 Установка браузеров Playwright..."
-python -m playwright install --with-deps
+echo "🚀 Проверка браузера Playwright..."
+python -m playwright install chromium || echo "⚠️ Chromium уже установлен или не требуется"
 
 echo "🚀 Запуск Gunicorn..."
 exec gunicorn app:app --workers 1 --bind 0.0.0.0:8000
